0383f7a8d39be6b566203c9525e218761b95c1d3
Alberto Elorza <albertoelorza2002@gmail.com>
2023-07-31 00:01:59 +0200

    progess creating a simplified api

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,4 @@
-use crate::{HingeConsumer, Token, Result, HingeOutput, CollectionNode};
+use crate::{HingeConsumer, Token, Result, HingeOutput, CollectionNode, NamedNode, AlwaysTrueNode, GreedyNode};
 
 #[derive(Debug)]
 pub struct Hinge(Box<dyn HingeConsumer>);
@@ -28,9 +28,50 @@ impl<T : HingeConsumer + 'static> From<T> for Hinge {
 }
 
 #[derive(Debug, Clone)]
+pub enum FlagType {
+  Bool,
+  Item,
+  List(usize),
+  RemainingItems
+}
+
+#[derive(Debug, Clone)]
 pub struct HingeBuilder(CollectionNode);
 
 impl HingeBuilder {
+  pub fn new() -> Self {
+    HingeBuilder(CollectionNode::new())
+  }
+
+  pub fn flag(
+    mut self,
+    name: impl AsRef<str>,
+    long: Option<impl AsRef<str>>,
+    short: Option<char>,
+    flag_config: FlagType
+  ) -> Self {
+    let name_options = [long.map(|x| format!("--{}", x.as_ref())), short.map(|x| format!("-{}", x))];
+    let names = name_options.iter().filter_map(|x| x.as_ref()).collect();
+    let node: NamedNode = match flag_config {
+      FlagType::Bool => NamedNode::new(names, AlwaysTrueNode),
+      FlagType::Item => NamedNode::new(names, GreedyNode::new()),
+      FlagType::List(count) => NamedNode::new(names, GreedyNode::new().accept_many(Some(count))),
+      FlagType::RemainingItems => NamedNode::new(names, GreedyNode::new().accept_many(None)),
+    };
+    self.0 = self.0.put(name, node);
+    self
+  }
+
+  pub fn require(mut self) -> Self {
+    self.0 = self.0.require_last();
+    self
+  }
+
+  pub fn catch_tail(mut self, name: impl AsRef<str>) -> Self {
+    self.0 = self.0.put(name, NamedNode::new(vec!["--"], GreedyNode::new().accept_many(None)));
+    self
+  }
+
   pub fn build(self) -> Hinge {
     self.0.into()
   }
